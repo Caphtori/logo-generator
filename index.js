2e5544aed3ee00d9f56588f8cee1de7c0893dfe4
Qohelet <benjy.shuter@gmail.com>
2023-10-03 02:26:10 -0400

    Adds questions for inquirer.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const fs = require("fs").promises;
 const inquirer = require("inquirer");
+
+const questions = require("./lib/questions.js");
+
+async function questioner(){
+    const result = await inquirer.prompt(questions).then((data)=>data)
+    return result;
+};
