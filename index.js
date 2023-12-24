38f548e59268470eee1af16adc48292a26cb57a5
Qohelet <benjy.shuter@gmail.com>
2023-12-23 19:44:42 -0500

    Adds a function to write the file.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs").promises;
 const inquirer = require("inquirer");
 const { renderElements } = require("./lib/elementConstructor.js");
+const { LogoName } = require('./lib/logoName.js')
 
 const questions = require("./lib/questions.js");
 
@@ -9,9 +10,15 @@ async function questioner(){
     return result;
 };
 
+function writeToFile(filename, data) {
+    fs.writeFile(`/output/${filename}`, renderElements(data)).then((err) =>
+    err ? console.log(err) : console.log('Success!'));
+};
+
 async function init(){
     const result = await questioner();
-    
+    const logoName = new LogoName(result.logoName);
+    writeToFile(logoName.filename, result);
 };
 
 init();
